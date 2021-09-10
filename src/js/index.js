c5d006230f5e92e17a74367315880dc3d1cf74ed
Israel Adefidipe <israeladefidipe@gmail.com>
2021-09-10 22:54:22 +0100

    added spinner feature and search filter now live

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,16 @@
 
 'use strict'
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const filterSelect = document.querySelector('.filter-select')
 const filterOptions = document.querySelector('.filter-options')
 const themeController = document.querySelector('.theme-controller')
 const body = document.body
+const cardContainer = document.querySelector('.card-container')
+const searchInput = document.querySelector('.search-input-field')
+const countryCard = document.querySelectorAll('.country-card')
 
 //  Dropdown
 filterSelect.addEventListener('click', () => {
@@ -43,6 +49,85 @@ themeController.addEventListener('click', () => {
     }else{
         disableDarkmode();
     }
+    
+})
+
+//SPINNER 
+const spinner = (parentElement) => {
+    const markup = `
+    <div class="spinner-container">
+            <div class="spinner">
+              <div class="spinner__spin">
+                  <i class="fas fa-spinner"></i>
+              </div>
+            </div>
+          </div>
+    `
+    parentElement.innerHTML = "";
+    parentElement.insertAdjacentHTML('afterbegin', markup);
+}
+
+// rendering country data 
+const countryData = async () => {
+    try{
+        spinner(cardContainer)
+
+        const response = await fetch('https://restcountries.eu/rest/v2/all')
+        
+        if(!response.ok) throw new Error ('Could not fetch country data, Please refresh page')
+        const data = await response.json()
+
+        
+        const markup = data.map(country =>
+             `
+            <a href="${country.alphaCode}" class="country-card">               
+                    <div class="country-image">
+                        <img src="${country.flag}" alt="${country.name}-flag">
+                    </div>
+                    <div class="country-description">
+                        <h2 class="country-name">${country.name}</h2>
+                        <p> <span>Population:</span>${country.population}</p>
+                        <p> <span>Region:</span>${country.region}</p>
+                        <p> <span>Capital:</span>${country.capital}d</p>
+                    </div>       
+            </a>
+            `).join('')
+
+            cardContainer.innerHTML = "";
+        cardContainer.insertAdjacentHTML('afterbegin', markup);
+
+    return markup
+
+
+
+    }
+    catch(err){
+        alert(err)
+    }
+
+}
+
+countryData()
+
+
+
+// search input
+searchInput.addEventListener('input', (e) =>{
+    const {value} = e.target
+
+    // console.log(countryCard)
+    const countryName = document.querySelectorAll('.country-name')
+    // console.log(countryName)
+
+    countryName.forEach(name => {
+        if(name.textContent.toLowerCase().includes(value.toLowerCase())){
+           name.closest('.country-card').style.display = 'block';
+        }
+        else{
+           name.closest('.country-card').style.display = 'none';
+
+        }
+    })
     
 })
 
